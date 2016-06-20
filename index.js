897a351afa5cf0e46da9bf7b1a9aa3d856227817
Patrick Smith <pgwsmith@gmail.com>
2016-06-20 15:59:24 +1000

    Add exported functionality

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const parseEnum = require('./parseEnum')
+const codeForKind = require('./code-generation/kind')
+const codeForJSON = require('./code-generation/json')
+const { equatesFuncForEnum } = require('./code-generation/equatable')
+
+export class Enum {
+	constructor(string) {
+		this.enumTree = parseEnum(string)
+	}
+
+	generateEquatesFunc() {
+		return equatesFuncForEnum(this.enumTree).join('\n')
+	}
+}
+
 const SwiftKick = {
-	
+	Enum
 }
